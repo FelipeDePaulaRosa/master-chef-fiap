48d2f31d9daba7f0c4ed0bdb2d0449e007a3a9fd
felipe.rosa <felipe.rosa@tinnova.com.br>
2023-11-01 16:28:04 -0300

    Implemented Identity and configured Migrations, and created, and created method to register Users

---
+++ src/Api/Master.Chef.Fiap.Api/Controllers/IdentityController.cs
@@ -0,0 +1,25 @@
+﻿using Master.Chef.Fiap.Application.AppServices.Identities;
+using Master.Chef.Fiap.Application.Dtos.Identities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Master.Chef.Fiap.Api.Controllers;
+
+public class IdentityController : MainController
+{
+    private readonly IIdentityAppService _appService;
+    
+    public IdentityController(IIdentityAppService appService)
+    {
+        _appService = appService;
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult<RegisterUserResponseDto>> Register(RegisterUserDto dto)
+    {
+        var result = await _appService.Register(dto);
+        if (!result.Success)
+            return BadRequest(result.Errors);
+        
+        return CreatedAtAction(nameof(Register), result);
+    }
+}
---
+++ src/Application/Master.Chef.Fiap.Application/AppServices/Identities/IIdentityAppService.cs
@@ -0,0 +1,8 @@
+﻿using Master.Chef.Fiap.Application.Dtos.Identities;
+
+namespace Master.Chef.Fiap.Application.AppServices.Identities;
+
+public interface IIdentityAppService
+{
+    Task<RegisterUserResponseDto> Register(RegisterUserDto dto);
+}
