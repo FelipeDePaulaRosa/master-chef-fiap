2170a536ef93635e40e3e41fb13ba37469c61b0b
felipe.rosa <felipe.rosa@tinnova.com.br>
2023-11-01 00:11:18 -0300

    Removed all projects

--- src/Application/Master.Chef.Fiap.Application/AppServices/Recipes/IRecipeAppService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Master.Chef.Fiap.Application.Dtos;
-
-namespace Master.Chef.Fiap.Application.AppServices;
-
-public interface IRecipeAppService
-{
-    Task<GetAllRecipesDto> GetAllRecipesAsync();
-    
-}
--- src/Application/Master.Chef.Fiap.Application/Dtos/GetAllRecipesDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Master.Chef.Fiap.Application.Dtos;
-
-public class GetAllRecipesDto
-{
-    public Guid Id { get; set; }
-    public string Title { get; set; }
-    public string Description { get; set; }
-}
