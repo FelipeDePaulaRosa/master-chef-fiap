b1afd394f9566850bcf5ebbd2c7cdcb11af3aa0e
felipe.rosa <felipe.rosa@tinnova.com.br>
2023-11-01 00:12:42 -0300

    Re added projects

---
+++ src/Application/Master.Chef.Fiap.Application/AppServices/Recipes/RecipeAppService.cs
@@ -0,0 +1,18 @@
+﻿using Master.Chef.Fiap.Application.Dtos;
+
+namespace Master.Chef.Fiap.Application.AppServices;
+
+public class RecipeAppService : IRecipeAppService
+{
+    public Task<GetAllRecipesDto> GetAllRecipesAsync()
+    {
+        var dto = new GetAllRecipesDto
+        {
+            Id = Guid.NewGuid(),
+            Title = "Recipe Title",
+            Description = "Recipe Description"
+        };
+        
+        return Task.FromResult(dto);
+    }
+}
